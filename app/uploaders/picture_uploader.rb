723d2dfc5f4f45da4a6d05f980654f98f3028ad0
Mai Inoue <pfh12nuj@s.okayama-u.ac.jp>
2018-02-18 02:45:24 -0500

    Change thibmnails size of 150 from 50

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -3,7 +3,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
   version :thumb do
-    process :resize_to_fill => [50, 50]
+    process :resize_to_fill => [150, 150]
   end
 
   # Choose what kind of storage to use for this uploader:
